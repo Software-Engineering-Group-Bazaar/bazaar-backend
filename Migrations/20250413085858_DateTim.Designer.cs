08ea50666a81d600bc79931b2a85f7fe32a837f9
lilhast1 <hastortarik4@gmail.com>
2025-04-13 10:59:15 +0200

    zab sam migraciju komitat

--- Migrations/20250413085858_DateTim.Designer.cs
+++ Migrations/20250413085858_DateTim.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Users.Models;
@@ -11,9 +12,11 @@ using Users.Models;
 namespace bazaar.Migrations
 {
     [DbContext(typeof(UsersDbContext))]
-    partial class UsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250413085858_DateTim")]
+    partial class DateTim
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
