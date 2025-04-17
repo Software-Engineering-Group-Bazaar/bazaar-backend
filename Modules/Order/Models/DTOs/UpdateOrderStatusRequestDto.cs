8012d970c980e3d10d185cc308d25607de1e2aae
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2025-04-17 10:49:33 +0200

    DTO for order - update status

---
+++ Modules/Order/Models/DTOs/UpdateOrderStatusRequestDto.cs
@@ -0,0 +1,8 @@
+namespace Order.Models.DTOs
+{
+    public class UpdateOrderStatusRequestDto
+    {
+        public int OrderId { get; set; }
+        public string NewStatus { get; set; }
+    }
+}
