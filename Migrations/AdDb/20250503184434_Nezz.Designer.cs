ef1864c1a91ab3a92ad842293d6c20a7ba0ab21d
Tarik Hastor <thastor1@etf.unsa.ba>
2025-05-03 20:48:09 +0200

    ja sam gljup

--- Migrations/AdDb/20250503184434_Nezz.Designer.cs
+++ Migrations/AdDb/20250503184434_Nezz.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MarketingAnalytics.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace bazaar.Migrations.AdDb
 {
     [DbContext(typeof(AdDbContext))]
-    partial class AdDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250503184434_Nezz")]
+    partial class Nezz
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
