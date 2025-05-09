2129be79ed63ec8ff803f0e36048886ef61c3caa
Tarik Hastor <thastor1@etf.unsa.ba>
2025-05-09 21:32:38 +0200

    spremno za koristenje

---
+++ Migrations/AdDb/20250509193219_Binarnakodiranja.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace bazaar.Migrations.AdDb
+{
+    /// <inheritdoc />
+    public partial class Binarnakodiranja : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductCategoryId",
+                table: "Advertisments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Triggers",
+                table: "Advertisments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "UserWeights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Weights = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWeights", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserWeights");
+
+            migrationBuilder.DropColumn(
+                name: "ProductCategoryId",
+                table: "Advertisments");
+
+            migrationBuilder.DropColumn(
+                name: "Triggers",
+                table: "Advertisments");
+        }
+    }
+}
