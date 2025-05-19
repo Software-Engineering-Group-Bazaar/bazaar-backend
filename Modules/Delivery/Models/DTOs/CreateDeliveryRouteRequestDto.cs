d855f7364cefccb1c36419b3517e4f74917ba6ab
Benjamin Baždar <103105206+Dzombi@users.noreply.github.com>
2025-05-19 23:48:16 +0200

    you can ask the flowers

--- Modules/Delivery/Models/DTOs/CreateDeliveryRouteRequestDto.cs
+++ Modules/Delivery/Models/DTOs/CreateDeliveryRouteRequestDto.cs
@@ -10,8 +10,5 @@ namespace Delivery.Dtos
         [Required]
         [MinLength(1, ErrorMessage = "At least one OrderId must be provided.")]
         public List<int> OrderIds { get; set; } = new List<int>();
-
-        [Required]
-        public DeliveryRouteDataDto RouteData { get; set; } = new DeliveryRouteDataDto();
     }
 }
